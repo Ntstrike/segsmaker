fc761f1b80b547a3b2666d43b04a4357b447a928
Ntstrike <98230980+Ntstrike@users.noreply.github.com>
2025-05-04 03:04:30 -0300

    Update setup.py
    Path append test fix

--- script/SM/setup.py
+++ script/SM/setup.py
@@ -18,7 +18,7 @@ import shlex
 import json
 import os
 import sys
-sys.path.append('/Segsmaker/script/nenen88')  # caminho correto onde o nenen88.py está
+sys.path.append('/segsmaker/script/nenen88')  # caminho correto onde o nenen88.py está
 from nenen88 import pull, say, download, clone, tempe
 
 SyS = get_ipython().system
